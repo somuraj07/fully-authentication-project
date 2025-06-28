ba3edfc760e471dfb8d22b1fc5961c1e3aa87ee8
somuraj07 <somasankar33@gmail.com>
2025-06-28 11:07:50 +0530

    backed part completed

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import authRouter from './routes/authRoute.js'
+import userRouter from "./routes/userRoutes.js";
 
 
 const app = express();
@@ -16,8 +17,11 @@ app.use(cors({credentials:true}))
 
 //end point of api
 app.get("/",(req,res)=>{
+    console.log(req.body);
     res.send("api is running hello ")
 })
 app.use('/api/auth',authRouter)
+app.use('/api/user',userRouter)
+
 
 app.listen(port,()=> console.log("server is started "));
